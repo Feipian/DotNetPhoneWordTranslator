47685652229a55a0675a5f6243f254874c01f262
HongMing <superwang0603@gmail.com>
2024-01-30 18:43:06 +0800

    Implement PhoneWord Translator using DotNet

---
+++ BestLearn/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BestLearn
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ BestLearn/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿namespace BestLearn
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnCall(object sender, EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                "Dial a Number",
+                "Would you like to call" + translatedNumber + "?",
+                "Yes",
+                "No"))
+            {
+                // Todo: dial the phone
+                try
+                {
+                    if (PhoneDialer.Default.IsSupported)
+                        PhoneDialer.Default.Open(translatedNumber);
+                }
+                catch (ArgumentException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone number was not valid", "OK");
+
+                }
+                catch (Exception) {
+                    await DisplayAlert("Unable to Dial", "Phone Dialing failed", "OK");
+                }
+
+
+            }
+        }
+
+        string translatedNumber;
+        
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = PhoneNumberText.Text;
+            translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
+
+            if (!string.IsNullOrEmpty(translatedNumber) )
+            {
+                CallButton.IsEnabled = true;
+                CallButton.Text = "Call" + translatedNumber;
+            }
+            else
+            {
+                CallButton.IsEnabled= false;
+                CallButton.Text = "Call";
+            }
+        }
+
+    }
+}
